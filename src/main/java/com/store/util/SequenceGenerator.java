7be024d54210f7364cbf91866c574eb92afa8cab
huanghz <huanghz@51kezhi.com>
2019-04-27 12:59:54 +0800

    序列

---
+++ src/main/java/com/store/util/SequenceGenerator.java
@@ -0,0 +1,43 @@
+package com.store.util;
+
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @ClassName: SequenceGenerator
+ * @Description: TODO
+ * @author: huanghz
+ * @date: 2019/4/27 11:53
+ */
+@Component
+public class SequenceGenerator
+{
+    private String store;
+    private long             min;
+    private long             max;
+    private long             counter;
+    private SimpleDateFormat format17;
+
+    public SequenceGenerator()
+    {
+        store = "2";
+        min = 10000L;
+        max = 99999L;
+        counter = 10000L;
+        format17 = new SimpleDateFormat("yyyyMMddHHmmssSSS");
+    }
+
+    // 获取序列号
+    public synchronized String next()
+    {
+        String dateStr = format17.format(new Date());
+        if (counter > max)
+        {
+            counter = min;
+        }
+        return store+dateStr + (counter++);
+    }
+
+}
